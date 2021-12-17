b5f76e53f5438be86410a9b993cdf899de7baa81
Oliver Malham <oliver.malham@gmail.com>
2021-12-17 16:15:54 +0000

    Adding proportional feedback control

--- wheel.cpp
+++ wheel.cpp
@@ -146,7 +146,7 @@ void WheelClass::trapezoid(void){
     distance += velocity;
   }
   distance_error = distance - distance_actual;
-  velocity_corrected = velocity + (distance_error * distance_co); // Very simple proportional control algorithm
+  velocity = velocity + (distance_error * distance_co); // Very simple proportional control algorithm
   update_motor();
 }
 
@@ -155,8 +155,8 @@ void WheelClass::triangle(void){
 }
 
 void WheelClass::update_motor(){
-  pwm = int(velocity_corrected * PWM_convert) + PWM_offset;
-  if(velocity_corrected <= 0.0) pwm = 0;
+  pwm = int(velocity * PWM_convert) + PWM_offset;
+  if(velocity <= 0.0) pwm = 0;
   if(pwm > PWM_max) pwm = PWM_max;
   if(pwm < 0) pwm = 0;
   
