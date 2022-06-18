5d7a5781e237a094d05959c8272833c0befc6818
Oliver Malham <oliver.malham@gmail.com>
2022-06-18 16:16:47 +0100

    Debugging comms

--- marvin_motion.cpp
+++ marvin_motion.cpp
@@ -103,6 +103,7 @@ void loop() {
   if(!in_motion) {
     //command_buffer_print();
     currentCommand = command_next();
+    printf("currentCommand: %u\n", (unsigned int)currentCommand);
 
     if(currentCommand != NULL){
 
